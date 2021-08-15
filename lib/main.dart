48e6b48958089d730a18f32ad3e44b99cba871ad
Yoga Meleniawan Pamungkas <64576201+yogameleniawan@users.noreply.github.com>
2021-08-15 19:35:10 +0700

    add tab bar view by index

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: Color(0xFFF8F8F8),
       ),
       home: HomePage(),
     );
@@ -27,6 +28,16 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  TabController? _tabController;
+  TextEditingController _inputSearch = new TextEditingController();
+  int _currentIndex = 0;
+
+  void onTabTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -57,41 +68,235 @@ class _HomePageState extends State<HomePage> {
             ),
           ],
         ),
-        body: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        body: DefaultTabController(
+          length: 6,
+          child: SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                child: Column(
                   children: [
-                    Text(
-                      "Select Category",
-                      style:
-                          TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "Select Category",
+                          style: TextStyle(
+                              fontSize: 25, fontWeight: FontWeight.bold),
+                        ),
+                        InkWell(
+                            onTap: () {},
+                            child: Padding(
+                              padding: const EdgeInsets.only(right: 5),
+                              child: Text("view all",
+                                  style: TextStyle(
+                                      fontSize: 15, color: Color(0xFFFF6E4E))),
+                            )),
+                      ],
                     ),
-                    InkWell(
-                        onTap: () {},
-                        child: Padding(
-                          padding: const EdgeInsets.only(right: 5),
-                          child: Text("view all",
-                              style: TextStyle(
-                                  fontSize: 15, color: Color(0xFFFF6E4E))),
-                        )),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 10),
+                      child: TabBar(
+                          onTap: onTabTapped,
+                          controller: _tabController,
+                          isScrollable: true,
+                          indicatorColor: Colors.transparent,
+                          unselectedLabelColor: Colors.black,
+                          labelColor: Color(0xFFFF6E4E),
+                          tabs: [
+                            Padding(
+                              padding:
+                                  const EdgeInsets.only(top: 10, bottom: 10),
+                              child: Tab(
+                                icon: CircleAvatar(
+                                  radius: 23,
+                                  backgroundColor: _currentIndex == 0
+                                      ? Color(0xFFFF6E4E)
+                                      : Colors.white,
+                                  child: Icon(
+                                    Icons.phone_android,
+                                    color: _currentIndex == 0
+                                        ? Colors.white
+                                        : Colors.grey,
+                                  ),
+                                ),
+                                text: "Phones",
+                              ),
+                            ),
+                            Padding(
+                              padding:
+                                  const EdgeInsets.only(top: 10, bottom: 10),
+                              child: Tab(
+                                icon: CircleAvatar(
+                                  radius: 23,
+                                  backgroundColor: _currentIndex == 1
+                                      ? Color(0xFFFF6E4E)
+                                      : Colors.white,
+                                  child: Icon(
+                                    Icons.computer,
+                                    color: _currentIndex == 1
+                                        ? Colors.white
+                                        : Colors.grey,
+                                  ),
+                                ),
+                                text: "Computer",
+                              ),
+                            ),
+                            Padding(
+                              padding:
+                                  const EdgeInsets.only(top: 10, bottom: 10),
+                              child: Tab(
+                                icon: CircleAvatar(
+                                  radius: 23,
+                                  backgroundColor: _currentIndex == 2
+                                      ? Color(0xFFFF6E4E)
+                                      : Colors.white,
+                                  child: Icon(
+                                    Icons.sports_motorsports,
+                                    color: _currentIndex == 2
+                                        ? Colors.white
+                                        : Colors.grey,
+                                  ),
+                                ),
+                                text: "Bike",
+                              ),
+                            ),
+                            Padding(
+                              padding:
+                                  const EdgeInsets.only(top: 10, bottom: 10),
+                              child: Tab(
+                                icon: CircleAvatar(
+                                  radius: 23,
+                                  backgroundColor: _currentIndex == 3
+                                      ? Color(0xFFFF6E4E)
+                                      : Colors.white,
+                                  child: Icon(
+                                    Icons.house,
+                                    color: _currentIndex == 3
+                                        ? Colors.white
+                                        : Colors.grey,
+                                  ),
+                                ),
+                                text: "House",
+                              ),
+                            ),
+                            Padding(
+                              padding:
+                                  const EdgeInsets.only(top: 10, bottom: 10),
+                              child: Tab(
+                                icon: CircleAvatar(
+                                  radius: 23,
+                                  backgroundColor: _currentIndex == 4
+                                      ? Color(0xFFFF6E4E)
+                                      : Colors.white,
+                                  child: Icon(
+                                    Icons.health_and_safety,
+                                    color: _currentIndex == 4
+                                        ? Colors.white
+                                        : Colors.grey,
+                                  ),
+                                ),
+                                text: "Health",
+                              ),
+                            ),
+                            Padding(
+                              padding:
+                                  const EdgeInsets.only(top: 10, bottom: 10),
+                              child: Tab(
+                                icon: CircleAvatar(
+                                  radius: 23,
+                                  backgroundColor: _currentIndex == 5
+                                      ? Color(0xFFFF6E4E)
+                                      : Colors.white,
+                                  child: Icon(
+                                    Icons.tv_sharp,
+                                    color: _currentIndex == 5
+                                        ? Colors.white
+                                        : Colors.grey,
+                                  ),
+                                ),
+                                text: "Electronic",
+                              ),
+                            ),
+                          ]),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 10),
+                      child: Row(
+                        children: <Widget>[
+                          Flexible(
+                            child: Padding(
+                              padding: const EdgeInsets.only(left: 20),
+                              child: TextFormField(
+                                controller: _inputSearch,
+                                keyboardType: TextInputType.text,
+                                decoration: InputDecoration(
+                                    enabledBorder: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(25.0),
+                                      borderSide: BorderSide(
+                                        color: Colors.white,
+                                      ),
+                                    ),
+                                    focusedBorder: OutlineInputBorder(
+                                      borderSide: const BorderSide(
+                                          color: Color(0xFFFF6E4E), width: 2.0),
+                                      borderRadius: BorderRadius.circular(25.0),
+                                    ),
+                                    border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(30.0),
+                                    ),
+                                    filled: true,
+                                    hintStyle:
+                                        TextStyle(color: Colors.grey[800]),
+                                    hintText: "Type in your text",
+                                    fillColor: Colors.white70,
+                                    prefixIcon: Icon(
+                                      Icons.search,
+                                      color: Color(0xFFFF6E4E),
+                                    )),
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: InkWell(
+                              onTap: () {},
+                              child: CircleAvatar(
+                                radius: 23,
+                                backgroundColor: Color(0xFFFF6E4E),
+                                child: Icon(Icons.grid_view_sharp,
+                                    color: Colors.white),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 20),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "Hot Sales",
+                            style: TextStyle(
+                                fontSize: 25, fontWeight: FontWeight.bold),
+                          ),
+                          InkWell(
+                              onTap: () {},
+                              child: Padding(
+                                padding: const EdgeInsets.only(right: 5),
+                                child: Text("see more",
+                                    style: TextStyle(
+                                        fontSize: 15,
+                                        color: Color(0xFFFF6E4E))),
+                              )),
+                        ],
+                      ),
+                    ),
                   ],
                 ),
-                ListView(
-                  shrinkWrap: true,
-                  physics: NeverScrollableScrollPhysics(),
-                  scrollDirection: Axis.horizontal,
-                  children: <Widget>[
-                    Text("tes"),
-                    Text("tes"),
-                    Text("tes"),
-                    Text("tes"),
-                  ],
-                )
-              ],
+              ),
             ),
           ),
         ));
